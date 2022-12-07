429ca7cc0e7040e11e5d03fd86fa7e2e9744fa33
Tim van de Wetering <45339455+timeester3648@users.noreply.github.com>
2022-12-07 15:23:05 +0100

    Update submodule

--- premake5.lua
+++ premake5.lua
@@ -172,24 +172,5 @@ project "KTX-Software"
 	filter { "options:popcnt" }
 		defines { "__POPCNT__" }
 
- 	filter "system:windows"
+ 	filter "action:vs*"
 		disablewarnings { "4244", "4996", "4005", "4267" }
-
- 	filter "configurations:Debug"
-		defines { "MLE_DEBUG_BUILD", "DEBUG" }
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines { "MLE_RELEASE_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
-
-	filter "configurations:Distribution"
-		defines {  "MLE_DISTRIBUTION_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
